96a17b73bb8ba1344f28104a315035dcbc65fe10
yair <yairmasterlol@gmail.com>
2023-12-17 20:29:12 -0600

    fixed

---
+++ README.md
@@ -0,0 +1,4 @@
+1. te recomendaria nombrar tu archivo js como __main.js__ o __index.js__
+no es obligatorio pero es una buena práctica.
+2. instalar los paquetes en lugar de usar CDN (ya coloque los link con su repectiva ruta a la carpeta).
+3.la propiedad __z-index__ solo funciona con elementos cuya posicion es dada por __relative,absolute,flex box, etc__. Teniendo esto en cuenta ya se aplicó a tu swiper y tiene el comportamiento esperado con solo CSS
--- javaScript/js.js
+++ javaScript/js.js
@@ -1,26 +1,28 @@
-const menu = document.querySelector(".menu");
-const openMenuBtn = document.querySelector(".open-menu");
-const closeMenuBtn = document.querySelector(".close-menu");
-const swiperContainer = document.querySelector('.swiper');
+/* no veo necesario el siguiente codigo solo consume recursos demás
+sobre todo los listeners
+*/
+// const menu = document.querySelector(".menu");
+// const openMenuBtn = document.querySelector(".open-menu");
+// const closeMenuBtn = document.querySelector(".close-menu");
+// const swiperContainer = document.querySelector('.swiper');
 
 
-function toggleMenu() {
-  menu.classList.toggle("menu_opened");
 
-  var mySwiper = swiperContainer.swiper
+// function toggleMenu() {
+//   menu.classList.toggle("menu_opened");
 
-    if (menu.classList.contains("menu_opened")) {
-        mySwiper.autoplay.stop();
-        mySwiper.allowTouchMove = false; /* Deshabilita el deslizamiento del swiper */
-        swiperContainer.classList.add("hidden"); 
-    } else {
-        mySwiper.autoplay.start();
-        mySwiper.allowTouchMove = true; /* Habilita el deslizamiento del swiper */
-        swiperContainer.classList.remove("hidden"); 
-    }
+//   var mySwiper = swiperContainer.swiper
+
+//     if (menu.classList.contains("menu_opened")) {
+//         mySwiper.autoplay.stop();
+//         mySwiper.allowTouchMove = false; /* Deshabilita el deslizamiento del swiper */
+//     } else {
+//         mySwiper.autoplay.start();
+//         mySwiper.allowTouchMove = true; /* Habilita el deslizamiento del swiper */
+//     }
   
-}
+// }
 
 
-openMenuBtn.addEventListener("click", toggleMenu);
-closeMenuBtn.addEventListener("click", toggleMenu);
+// openMenuBtn.addEventListener("click", toggleMenu);
+// closeMenuBtn.addEventListener("click", toggleMenu);
